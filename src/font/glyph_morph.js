cab907f45d36056df7e6d9b522d19abc5356edbd
JuanFuent.es <input@juanfuent.es>
2024-12-23 23:20:18 -0600

    Add gsap yoyo transition

--- src/font/glyph_morph.js
+++ src/font/glyph_morph.js
@@ -1,3 +1,5 @@
+gsap.registerPlugin(MorphSVGPlugin)
+
 import Glyph from './glyph'
 
 export default class GlyphMorph {
@@ -8,6 +10,17 @@ export default class GlyphMorph {
         this.fontSize = 100
         this.minGlyph = this.createGlyph(this.getMinVariation())
         this.maxGlyph = this.createGlyph(this.getMaxVariation())
+    }
+
+    animate() {
+        // Requires GSAP license to work with MorphSVGPlugin on production
+        gsap.to("#start", {
+            duration: 2,
+            morphSVG: "#end",
+            repeat: -1,
+            yoyo: true,
+            ease: "power1.inOut"
+        });
     }
 
     // Get the minimum variation of the glyph
@@ -40,31 +53,29 @@ export default class GlyphMorph {
         })
     }
 
-    draw() {
-        const t = Math.sin(Date.now() * 0.001) * 0.5 + 0.5
-    }
-
     appendSVG() {
         const svgNS = 'http://www.w3.org/2000/svg';
         const svg = document.createElementNS(svgNS, 'svg');
         svg.setAttribute('width', this.maxGlyph.width);
         svg.setAttribute('height', this.maxGlyph.lineHeight);
-        svg.setAttribute('fill', "#FFF");
         svg.setAttribute('viewBox', `0 0 ${this.maxGlyph.width} ${this.maxGlyph.lineHeight}`);
-
-        let path = this.createSVGPath(this.minGlyph.svgPathData)
-        svg.appendChild(path);
-        let path2 = this.createSVGPath(this.maxGlyph.svgPathData)
-        svg.appendChild(path2)
-
+        
+        this.path_a = this.createSVGPath(this.minGlyph.svgPathData, "#FFF", "start")
+        svg.appendChild(this.path_a)
+        //
+        this.path_b = this.createSVGPath(this.maxGlyph.svgPathData, "transparent", "end")
+        svg.appendChild(this.path_b)
+        //
         document.body.appendChild(svg)
     }
-
-
-    createSVGPath(_svgPathData) {
+    
+    
+    createSVGPath(_svgPathData, _fill = "#FFF", _id = "start") {
         const svgNS = 'http://www.w3.org/2000/svg';
         const path = document.createElementNS(svgNS, 'path');
+        path.setAttribute('id', _id);
         path.setAttribute('d', _svgPathData);
+        path.setAttribute('fill', _fill);
         path.setAttribute('transform', `translate(0, ${this.maxGlyph.lineHeight})`)
         return path
     }
--- src/main.js
+++ src/main.js
@@ -1,17 +1,18 @@
 import './style.css'
 import VariableFont from './font/variable_font'
 import GlyphMorph from './font/glyph_morph'
-class Variable {
+class VariableSVG {
     constructor(_container) {
         this.font = new VariableFont('/HubotSans.ttf')
         this.font.addEventListener('fontloaded', () => this.setup())
     }
     
     setup() {
-        this.glyph_morph = new GlyphMorph('W', this.font)
+        this.glyph_morph = new GlyphMorph('R', this.font)
         this.glyph_morph.appendSVG()
+        this.glyph_morph.animate()
     }
 
 }
 
-const variation = new Variable()
+const variation = new VariableSVG()
